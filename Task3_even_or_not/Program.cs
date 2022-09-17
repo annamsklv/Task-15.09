25d87f34f163e83e28c5b59ca1355f3f736805cf
anna.msklv <anna.msklv@yandex.ru>
2022-09-17 15:39:57 +0300

    Task3_even_or_not

---
+++ Task3_even_or_not/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Введите число");
+var input = Console.ReadLine();
+int number = Int32.Parse(input);
+
+
+if(number%2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
